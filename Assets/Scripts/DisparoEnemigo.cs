ee32cad17a25d62038e7a7788d7ab1f57b39dd37
NehuenIniguez <164099712+NehuenIniguez@users.noreply.github.com>
2025-04-03 10:36:15 -0300

    Feature/logica disparo Enemigo

---
+++ Assets/Scripts/DisparoEnemigo.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoEnemigo : MonoBehaviour
+{
+    [SerializeField] private Transform ControlDisparo;
+    [SerializeField] private GameObject bala;
+    [SerializeField] private float Velocidadbala;
+    [SerializeField] private GameObject jugador;
+    [SerializeField] private float tiempoEntreDisparos = 1f;
+    private float tiempoUltimoDisparo;
+    private bool enRango = false;
+
+    void Start()
+    {
+        jugador = GameObject.FindGameObjectWithTag("Personaje");
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Personaje") && !enRango) 
+        {
+            enRango = true;
+            InvokeRepeating(nameof(Disparo), 0f, tiempoEntreDisparos);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Personaje") && enRango)
+        {
+            enRango = false;
+            CancelInvoke(nameof(Disparo));
+        }
+    }
+   // void Update()
+   // {
+   //     Vector2 direccion = (jugador.transform.position - transform.position).normalized;
+   //     if (Time.time > tiempoUltimoDisparo + tiempoEntreDisparos)
+   //     {
+   //         Disparo(direccion);
+   //         tiempoUltimoDisparo = Time.time;
+   //     }
+   // }
+
+    public void Disparo()
+    {
+        if (jugador == null) return;
+        Vector2 direccion = (jugador.transform.position - transform.position).normalized;
+        GameObject proyectil = Instantiate(bala, ControlDisparo.position, Quaternion.identity);
+        Rigidbody2D rb = proyectil.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = direccion * Velocidadbala;
+        }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+       Gizmos.DrawWireSphere(ControlDisparo.position, 10f);
+    }
+}
